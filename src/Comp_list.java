09854e865fee64276aa2938d4a61d910a8e9126e
iris <irisregit@yahoo.com>
2020-02-19 23:01:43 +0500

    123

---
+++ src/Comp_list.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Comp_list extends JFrame{
+    private JPanel pn1;
+    private JList complist;
+    private JLabel lb1;
+    private JLabel lb2;
+    private JLabel lb3;
+    private JTextField title;
+    private JTextField location;
+    private JTextField c_date;
+    private JButton add;
+    private JButton upd;
+}
